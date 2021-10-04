ed388b90c380f8277d5cdb1051d9d0dcafa08410
martex84 <martex849@gmail.com>
2021-10-04 15:35:41 -0300

    Conclusao Estetica Produto

--- frontend/src/view/pages/home.jsx
+++ frontend/src/view/pages/home.jsx
@@ -11,21 +11,22 @@ function Home() {
 
     const [objetoProduto, setObjetoProduto] = useState(null);
     const [produtoInterno, setProdutoInterno] = useState(null);
+    const [categoriaAtual, setCategoriaAtual] = useState("Data");
 
-    const criarProduto = useCallback(() => {
+    const criarProduto = useCallback((objeto) => {
         const arrayInterna = [];
 
-        objetoProduto.map(campo => {
+        objeto.map(campo => {
             return arrayInterna.push(<Produto objeto={campo} />)
         })
 
         setProdutoInterno(arrayInterna)
-    }, [objetoProduto])
+    })
 
     useEffect(() => {
         if (objetoProduto === null) setObjetoProduto(jsonProduto);
 
-        if (produtoInterno === null && objetoProduto !== null) criarProduto();
+        if (produtoInterno === null && objetoProduto !== null) criarProduto(objetoProduto);
 
     }, [objetoProduto, produtoInterno, criarProduto])
 
@@ -37,8 +38,8 @@ function Home() {
             <Header />
             <Navbar />
             <section id="sectionPrincipalHome">
-                <div id="barraFiltro">
-                    Barra Filtro
+                <div id="barraFerramentaCentral">
+                    Barra Central
                 </div>
                 <div id="containerGrupoProdutos">
                     {produtoInterno}
