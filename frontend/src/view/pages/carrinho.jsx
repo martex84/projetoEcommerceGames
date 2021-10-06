9f1f69750b35d1e7cd04f62239fa85a1dd3b502b
martex84 <martex849@gmail.com>
2021-10-06 20:16:31 -0300

    Conclusao Pagina Carrinho 2.0

--- frontend/src/view/pages/carrinho.jsx
+++ frontend/src/view/pages/carrinho.jsx
@@ -18,16 +18,38 @@ function Carrinho() {
         total: 0,
     })
 
+    function limparCarrinho() {
+        localStorage.removeItem("itemCarrinho");
+
+        window.location.reload();
+    }
+
+    function concluirCompra() {
+        alert("Obrigado por comprar na EGame!");
+
+        limparCarrinho();
+    }
+
     function calcularValores() {
         let frete = 0;
         let subtotal = 0;
         let total = 0;
 
         valorLocalStorage.map(componente => {
-            subtotal = subtotal + componente.price;
             frete = frete + 10;
+            subtotal = subtotal + componente.price;
             total = frete + subtotal;
         })
+
+        if (subtotal > 250) {
+            frete = "Gratis!";
+        }
+        else {
+            frete = `R$ ${frete}`;
+        }
+
+        subtotal = `R$ ${subtotal}`;
+        total = `R$ ${total}`;
 
         setCalculoProdutos({
             frete,
@@ -96,7 +118,7 @@ function Carrinho() {
             <section id="sectionCarrinho">
                 <div id="containerGrupoProdutosCarrinho">
                     <h2 id="tituloGrupoProduto">
-                        Produto Carrinho
+                        Produtos do Carrinho
                     </h2>
                     <div id="grupoProdutosCarrinho">
                         {componenteProduto}
@@ -104,28 +126,32 @@ function Carrinho() {
                 </div>
                 <div id="containerResultadoCheckout">
                     <h2 id="tituloResultadoCheckout">
-                        Resumo
+                        Resumo da Compra
                     </h2>
                     <div id="dadosResultadoCarrinho">
                         <p className="informacaoResultado">
                             <span>{`Frete: `}</span>
-                            <span className="negrito">${calculoProdutos.frete}</span>
+                            <span className="negrito">{calculoProdutos.frete}</span>
                         </p>
                         <p className="informacaoResultado">
                             <span>{`SubTotal: `}</span>
-                            <span className="negrito">${calculoProdutos.subtotal}</span>
+                            <span className="negrito">{calculoProdutos.subtotal}</span>
                         </p>
                         <p className="informacaoResultado">
                             <span>{`Total: `}</span>
-                            <span className="negrito">${calculoProdutos.total}</span>
+                            <span className="negrito">{calculoProdutos.total}</span>
                         </p>
                     </div>
                     <div id="containerGrupoBotaoResultadoCarrinho">
                         <div className="containerBotaoResultadoCarrinho">
-                            <button className="botaoResultado botaoTotal">Concluir Compra</button>
+                            <button className="botaoResultado botaoTotal" onClick={e => concluirCompra()}>
+                                Concluir Compra
+                            </button>
                         </div>
                         <div className="containerBotaoResultadoCarrinho">
-                            <button className="botaoResultado botaoLimpar">Limpar Compra</button>
+                            <button className="botaoResultado botaoLimpar" onClick={e => limparCarrinho()}>
+                                Limpar Compra
+                            </button>
                         </div>
                     </div>
                 </div>
